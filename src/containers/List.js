dc453896a87239c2b714911b47d7effb9b18e024
Reky Senjaya <rekysenjaya@gmail.com>
2018-01-14 16:08:28 +0700

    change UI

--- src/containers/List.js
+++ src/containers/List.js
@@ -20,6 +20,54 @@ import Header from '../common/Header';
 
 const D = Dimensions.get('window')
 
+const style = {
+  row: {
+    backgroundColor: 'white',
+    padding: 2,
+    margin: 1,
+    borderWidth: 2,
+    borderColor: '#24292e',
+    flexDirection: 'column'
+  },
+  rowImage: {
+    padding: 3,
+    flexDirection: 'row'
+  },
+  image: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    borderRadius: 5
+  },
+  full_name: {
+    color: '#0366d6',
+    fontWeight: '900'
+  },
+  language: {
+    color: '#f1e05a',
+    fontWeight: '500'
+  },
+  description: {
+    padding: 3,
+    color: '#586069'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '600',
+    padding: 12
+  },
+  buttonColor: {
+    backgroundColor: '#586069'
+  },
+  spinner: {
+    color: '#FFF'
+  },
+  flatList: {
+    flex: 1,
+    marginHorizontal: 5
+  }
+}
+
 export class List extends Component {
   constructor(props) {
     super(props);
@@ -30,7 +78,7 @@ export class List extends Component {
   }
 
   handleSearch() {
-    if (this.refs.search._lastNativeText !== null) {
+    if (this.refs.search._lastNativeText !== null && this.refs.search._lastNativeText !== '') {
       this.props.dispatch(searchAction(this.refs.search._lastNativeText));
     }
   }
@@ -42,39 +90,43 @@ export class List extends Component {
 
   render() {
     const { data, loading, selectedRepo } = this.props;
-    
+
     return (
       <View style={{ flex: 1 }} >
         <Header />
-        <Spinner visible={loading} textContent={"Loading..."} textStyle={{color: '#FFF'}} />
-        <View style={{ flexDirection: 'row' }} >
+        <Spinner visible={loading} textContent={"Loading..."} textStyle={style.spinner} />
+        <View style={{
+          backgroundColor: 'white',
+          flexDirection: 'row',
+          borderBottomWidth: 0.5,
+          borderBottomColor: 'grey',
+          elevation: 2
+        }} >
           <TextInput style={{ flex: 1 }} placeholder="Search" ref="search" onBlur={(e) => this.handleSearch()} />
-          <TouchableOpacity style={{ backgroundColor: '#586069' }}
+          <TouchableOpacity style={style.buttonColor}
             onPress={() => this.handleClear()}>
-            <View style={{ padding: 12 }} >
-              <Text style={{ color: 'white', fontWeight: '600' }} >Clear</Text>
-            </View>
+            <Text style={style.buttonText} >Clear</Text>
           </TouchableOpacity>
         </View>
         {!loading &&
           <FlatList
-            style={{ flex: 1, margin: 5 }}
+            style={style.flatList}
             data={data}
             renderItem={({ item, index }) => {
               return (
                 <TouchableOpacity
-                  onPress={() => this.handleClear()}>
-                  <View style={{ backgroundColor: 'white', padding: 2, margin: 1, borderWidth: 2, borderColor: '#24292e', flexDirection: 'column' }}>
-                    <View style={{ padding: 3, flexDirection: 'row' }} >
+                  onPress={() => this.handleSelect(item)}>
+                  <View style={style.row}>
+                    <View style={style.rowImage} >
                       {(item.owner && item.owner.avatar_url) &&
-                        <Image source={{ uri: item.owner.avatar_url }} resizeMode="cover" style={{ width: 40, height: 40, marginRight: 10, borderRadius: 5 }} />
+                        <Image source={{ uri: item.owner.avatar_url }} resizeMode="cover" style={style.image} />
                       }
                       <View>
-                        <Text style={{ color: '#0366d6', fontWeight: '900' }} >{item.full_name ? item.full_name.length >= 35 ? `${item.full_name.substring(0, 35)}...` : item.full_name : ''}</Text>
-                        <Text style={{ color: '#f1e05a', fontWeight: '500' }} >{item.language || ''}</Text>
+                        <Text style={style.full_name} >{item.full_name ? item.full_name.length >= 35 ? `${item.full_name.substring(0, 35)}...` : item.full_name : ''}</Text>
+                        <Text style={style.language} >{item.language || ''}</Text>
                       </View>
                     </View>
-                    <Text style={{ padding: 3, color: '#586069' }} >{item.description || ''}</Text>
+                    <Text style={style.description} >{item.description || ''}</Text>
                   </View>
                 </TouchableOpacity>
               )
